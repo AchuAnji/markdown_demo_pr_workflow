13b526085c186be6d269f48e1087832c8bc6f680
Anjineyulu <achuanji.b@gmail.com>
2026-03-17 19:52:39 -0500

    sentry

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import streamlit as st
+import config
 
 st.title("Defect Dashboard")
 
 name = st.text_input("Enter your name")
+x = 1 / 0
 
 if name:
     st.write(f"Hello {name}")
--- config.py
+++ config.py
@@ -1,7 +1,18 @@
 from dotenv import load_dotenv
 import os
+import sentry_sdk
+import time
 
 load_dotenv()
+
+SENTRY_DSN = "https://25f297fe1d3410b54dd88565304607a5@o4511027157401600.ingest.us.sentry.io/4511027216515072"
+
+sentry_sdk.init(
+    dsn=SENTRY_DSN,
+    send_default_pii=False,
+    traces_sample_rate=0.0,
+)
+
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 TEST_DATABASE_URL = os.getenv("TEST_DATABASE_URL")
